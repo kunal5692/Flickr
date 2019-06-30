290afd95f95da7c534df1eeaf46f036555d4f0e0
Kunal <techcraze5@gmail.com>
2019-06-30 17:23:15 +0530

    Add photo cell implementation

--- Flickr/View/SearchScene/PhotosCell.swift
+++ Flickr/View/SearchScene/PhotosCell.swift
@@ -10,4 +10,35 @@ import UIKit
 
 class PhotosCell: UICollectionViewCell {
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var containerView: UIView!
+    
+    private var secret : String?
+    private var farm : String?
+    private var id : String?
+    private var server : String?
+    
+    var photoCellViewModel : PhotosCellViewModel? {
+        didSet{
+            secret = photoCellViewModel?.secret
+            farm = photoCellViewModel?.farm
+            id = photoCellViewModel?.id
+            server = photoCellViewModel?.server
+            self.loadImage()
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        debugPrint("Awake from NIB")
+    }
+    
+    func loadImage() {
+        debugPrint("Load images")
+        guard let url = URLBuilder.getImageFarmURL(farm: farm!, id: id!, secret: secret!, server: server!) else {
+            return
+        }
+        debugPrint("Loading image for url ", url.absoluteString)
+        self.imageView.downloadAndCacheImage(url: url)
+    }
 }
